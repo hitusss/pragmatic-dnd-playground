50eb7d790213816b98d603ff3b30e0b294feed6d
Hitusss <kuba.niespielak@gmail.com>
2025-04-17 14:04:56 +0200

    clenup home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
+import { Link } from "react-router";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Button } from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,24 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="grid place-content-center p-12 lg:p-24">
+      <h1>Pragmatic Drag and Drop Playground</h1>
+      <nav className="mt-16 px-8">
+        <h2>Examples:</h2>
+        <ul className="grid gap-2">
+          <li>
+            <Button
+              variant="link"
+              size="lg"
+              asChild
+              className="text-lg font-bold"
+            >
+              <Link to="">Example 1</Link>
+            </Button>
+          </li>
+        </ul>
+      </nav>
+    </main>
+  );
 }
